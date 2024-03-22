3356a2c1142b2e36f8de58c42bd9cd054c59a64d
kaydnnn <160500192+kaydnnn@users.noreply.github.com>
2024-03-22 06:15:25 +0000

    submission of assignment 4a

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 nric = input('Enter an NRIC number: ')
+possibleStartingLetters = ("T", "S", "F", "G")
 
-# Type your code below
+if (nric.upper().startswith(possibleStartingLetters)) and (
+len(nric) == 9) and (nric[1:8].isdigit()) and (nric[8:].isalpha()):
+    digitWeight = [2, 7, 6, 5, 4, 3, 2]
+    digits = list()
+    for i in nric[1:8]:
+        digits.append(int(i))
+    combined = (a*b for a,b in list(zip(digits, digitWeight)))
+    sumOfDigits = sum(combined)
+    if (nric[0] in "TGtg"):
+        sumOfDigits += 4
+    quotient, remainder = divmod(sumOfDigits, 11)
+    ST = ["J", "Z", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+    FG = ["X", "W", "U", "T", "R", "Q", "P", "N", "M", "L", "K"]
+    if (nric[0] in "STst") and (nric.endswith(ST[remainder])):
+        print("NRIC is valid.")
+    elif (nric[0] in "FGfg" and (nric.endswith(ST[remainder]))):
+        print("NRIC is valid.")
+    else:
+        print("NRIC is invalid.")
+else:
+    print("NRIC is invalid.")
